e85721a482da871d8dc27d17cab7ef88fad03902
Franco Seguel <francoseguellucero@gmail.com>
2022-10-14 13:29:00 -0300

    agregado el main en la parte uno

---
+++ parte_uno.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "utils.c"
+
+
+
+int main(int argc, char *argv[]){
+    double *d = distanciasInt(30);
+    double *p = previos(30);
+
+    destruirDistanciasDouble(d);
+    destruirPrevios(p);
+}
