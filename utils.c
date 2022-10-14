5ec0daa736d39523210efe09be4160519202ec37
Franco Seguel <francoseguellucero@gmail.com>
2022-10-14 11:41:26 -0300

    cambio de nombre a utils

---
+++ utils.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct nodo{
+    int peso;
+    Nodo *next;
+}Nodo;
+
+double *distanciasInt(int size){
+    double *dis = calloc(size, sizeof(double));
+
+    //debuggeando el error de calloc
+    if (dis == NULL){
+        perror("distanciasInt");
+        exit(-1);
+    }
+    // inicializando las distancias con ceros
+    for(int i = 0; i < size; i++){
+        dis[i] = 0;
+    }
+    return dis;
+}
+
+void destruirDistanciasInt(double *dis){
+    free(dis);
+}
+
+int previos(){
+
+}
