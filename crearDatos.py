ec140890e5228bb58f1dd3a3bd1802714f05bd64
Franco Seguel <francoseguellucero@gmail.com>
2022-10-18 17:04:53 -0300

    script de crear datos creado

---
+++ crearDatos.py
@@ -0,0 +1,12 @@
+import random
+import sys
+
+def crearAristas(size): # size es un entero
+    f = open(f"{size}.txt", "w")
+    linea = [str(random.randint(0, 2**14)) for i in range(0, size*2)]
+    linea = "\n".join(linea)
+    f.write(linea)
+    f.close()
+
+
+crearAristas(int(sys.argv[1]))
